7eaacaf36054c5d53b19643fbbf802582f362353
SPK80 <psukonkin@gmail.com>
2022-04-13 20:46:40 +0300

    refactor: delete infinite cicle in main

--- src/ru/spk80/calculator/Main.java
+++ src/ru/spk80/calculator/Main.java
@@ -11,17 +11,16 @@ class Main {
 	}
 
 	public static void main(String[] args) {
-
 		try (var scanner = new Scanner(System.in)) {
-			String input;
-			while (true) {
-				input = scanner.nextLine();
-				try {
-					System.out.println(calc(input));
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
+			var input = scanner.nextLine();
+
+			try {
+				var result = calc(input);
+				System.out.println(result);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
 			}
+
 		}
 	}
 }
