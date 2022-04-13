91aade67ea3d85dabcb2923d0842f91b437766f6
SPK80 <psukonkin@gmail.com>
2022-04-13 18:39:29 +0300

    fix: Roman

--- src/ru/spk80/calculator/Roman.java
+++ src/ru/spk80/calculator/Roman.java
@@ -1,15 +1,24 @@
 package ru.spk80.calculator;
 
 public class Roman {
-	private final String[] nums = { "N", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
-	private final String[] tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C" };
+	private static final String[] nums = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+	private static final String[] tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C" };
 
-	String toRoman(int value) throws Exception {
+	public static String toRoman(int value) throws Exception {
 		if (value < 1 || value > 100)
-			throw new Exception("Invalid value!");
+			throw new Exception("Invalid value: " + value);
 		var ten = value / 10;
 
-		return tens[ten] + nums[value];
+		return tens[ten] + nums[value % 10];
 	}
 
+	public static void main(String[] args) {
+		for (int i = 0; i <= 101; i++) {
+			try {
+				System.out.println(i + toRoman(i));
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
 }
