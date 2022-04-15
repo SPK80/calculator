0fec05f26de121651d5561568b3aba0bc27a1aa8
SPK80 <psukonkin@gmail.com>
2022-04-15 19:14:12 +0300

    refactor: main try - catch

--- src/ru/spk80/calculator/Main.java
+++ src/ru/spk80/calculator/Main.java
@@ -11,16 +11,15 @@ class Main {
 	}
 
 	public static void main(String[] args) {
+
 		try (var scanner = new Scanner(System.in)) {
+
 			var input = scanner.nextLine();
+			var result = calc(input);
+			System.out.println(result);
 
-			try {
-				var result = calc(input);
-				System.out.println(result);
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 	}
 }
