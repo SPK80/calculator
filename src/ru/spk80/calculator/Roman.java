ed91e754da8527e4465d5a4a2131fc8607adcac1
SPK80 <psukonkin@gmail.com>
2022-04-15 18:43:57 +0300

    refactor: Roman

--- src/ru/spk80/calculator/Roman.java
+++ src/ru/spk80/calculator/Roman.java
@@ -1,26 +1,30 @@
 package ru.spk80.calculator;
 
 class Roman {
+
 	private static final String[] nums = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" };
 	private static final String[] tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C" };
 
 	public static int toInt(String strValue) throws Exception {
-		var l = nums.length;
-		var i = 0;
+
+		int l = nums.length;
+		int i = 0;
 
 		while (!strValue.equals(nums[i]) && i++ < l - 1) {
 		}
 
 		if (i < 1 || i >= l)
-			throw new Exception("Invalid number " + strValue);
+			throw new Exception("Invalid value " + strValue);
+
 		return i;
 	}
 
 	public static String toRoman(int value) throws Exception {
-		if (value < 1 || value > 100)
-			throw new Exception("Invalid value: " + value);
-		var ten = value / 10;
 
+		if (value < 1 || value > 100)
+			throw new Exception("Value " + value + " out of range");
+
+		int ten = value / 10;
 		return tens[ten] + nums[value % 10];
 	}
 }
